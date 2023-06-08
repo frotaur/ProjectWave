d25642879ad77a998dea22b8d19fa63a9de659d2
Vassilis <vassilis.papadopoulos@epfl.ch>
2023-06-08 17:43:30 +0200

    Added chamfer loss, multisource working, but not great training

--- WaveModel.py
+++ WaveModel.py
@@ -3,15 +3,45 @@ import torch.nn as nn
 from torch.nn import functional as F
 from Block import Block
 
+def chamfer_dist(x, y):
+    """
+    Compute the Chamfer Distance between two sets of points
+    
+    Parameters:
+    x, y: torch.Tensor with shape [batch_size, n_points, coordinates]
+
+    Returns:
+    torch.Tensor with shape [batch_size], the batched Chamfer Distance
+    """
+    assert x.shape==y.shape
+    (B,N,D) = x.shape
+    # Compute pairwise distance matrices
+    # Divide by D because that's what mse loss does (averaging over all distances computed
+    pairdistsquared = ((x[:,:,None,:]-y[:,None,:,:])**2).mean(dim=-1) # (B,N,N)
+
+    # Find minimum distances
+    min_distxy,_ = torch.min(pairdistsquared, dim=1,keepdim=False) # (B,N) min distance way 1
+    min_distyx,_ = torch.min(pairdistsquared, dim=2, keepdim=False) # (B,N) min distance way 2
+
+    chamfer_dist =.5*(min_distxy.mean(dim=-1) + min_distyx.mean(dim=-1)) # (B,) distance between the two cloud points
+    
+    return chamfer_dist.mean() # Average over batch !
 
 
 class WaveModel(nn.Module):
+    """
+        Model that predicts mics location directly from the timeseries data.
+        Not clear yet what the best way to proceed is yet, but for now uses
+        the last 'token' embedding to make the prediciton of the source locations :
+    """
     def __init__(self,mic_num,source_num,tot_timesteps,n_embd,n_hidden,n_head,n_layer,dropout=0.2):
         super().__init__()
         #wavedata:(6,512,3) (3,6,512)(512 3*6) (2,0,1)(3,1,2)
         
         self.position_embedding_table = nn.Embedding(tot_timesteps, n_embd)
+
         self.val_embed = nn.Linear(mic_num,n_embd)
+        self.pos_embed = nn.Linear(mic_num*2,n_embd)
 
         self.blocks = nn.Sequential(*[Block(n_embd, n_head,n_hidden,dropout) for _ in range(n_layer)])
         self.ln_f = nn.LayerNorm(n_embd) # final layer norm（B,T,n_embd)
@@ -19,6 +49,8 @@ class WaveModel(nn.Module):
         self.lm_head = nn.Linear(n_embd, 2*source_num)
 
         self.tan=nn.Tanh()
+
+        self.source_num=source_num
         # better init, not covered in the original GPT video, but important, will cover in followup video
         self.apply(self._init_weights)
 
@@ -45,25 +77,25 @@ class WaveModel(nn.Module):
         #### HERE x should have shape (B,T,n_embed)
         idx=torch.permute(idx,(0,3,2,1)) # (B,T,M,3)
         vals = idx[...,0]
-        pos = idx[:,0,:,1:] #(B,M,2)
+        pos = idx[:,0,:,1:].reshape(B,M*2) #(B,M*2)
         idx=idx.reshape(B,T,M*3)
         # idx=torch.reshape(idx,(B,T*M*3))
         pos_emb = self.position_embedding_table(torch.arange(T, device= idx.device)[None].expand(B,-1))# (B,T,C)
 
-        idx=self.embedding_linear(idx)+pos_emb # (B,T,n_embed)
+        idx=self.val_embed(vals)+self.pos_embed(pos)[:,None,:]+pos_emb # (B,T,n_embed)
         
         idx = self.blocks(idx) # (B,T,C)
         idx = self.ln_f(idx) # (B,T,C) 
 
         B,T,C=idx.shape
-        #idx=idx.view(B,T*C)
-        idx = self.lm_head(idx[:,-1])#(B,2*n)
+        #idx=idx.reshape(B,T*C)
+        idx = self.lm_head(idx[:,-1]).reshape(B,self.source_num,2)#(B,n,2)
         predicts = torch.clamp(idx,min=0.,max=1.)
         
         #predicts = (self.tan(predicts)+1)/2
         # predicts=torch.permute(predicts,(0,2,1))
         ### CHANGES HERE AGAIN :
-        # logits = x.view(n,2) # (B,T,vocab_size)
+        # logits = x.reshape(n,2) # (B,T,vocab_size)
         ### We want to get an 'output' variable, which
         ### has size (B,N,2)
         # predicts = torch.permute(predicts,(0,2,1))
@@ -72,13 +104,9 @@ class WaveModel(nn.Module):
         if targets is None:
             loss = None
         else:
-            # predicts = predicts.view(B,2*N,T)
-            # B, T, C = logits.shape
-            # predicts = predicts.view(B,2*N)
-            targets = targets.view(B,2*N)
-            # loss = F.cross_entropy(logits, targets)
-            loss = F.mse_loss(predicts, targets)+F.mse_loss(predicts,idx)
-        ###
+            # loss = F.mse_loss(predicts,targets)+F.mse_loss(predicts,idx)
+            loss = chamfer_dist(predicts,targets)+F.mse_loss(predicts,idx)
+
         
         return predicts, loss
     
@@ -86,7 +114,7 @@ class WaveModel(nn.Module):
 class NoHeadModel(nn.Module):
     """
     Process the dataseries of mic recordings, but does not process the final output of the transformer
-    layer.
+    layer. This can be attached to any head to make either source predictions, or next token prediction.
     """
     def __init__(self,mic_num,tot_timesteps,n_embd,n_hidden,n_head,n_layer,dropout=0.2):
         super().__init__()
@@ -129,6 +157,11 @@ class NoHeadModel(nn.Module):
 
 
 class PredictionModel(nn.Module):
+    """
+        Uses a NoHeadModel coupled with a linear head that acts on every timestep T,
+        to produce predictions for the next token. 
+        <WARNING :  to be used WITH masking in the attention heads>
+    """
     def __init__(self,mic_num,tot_timesteps,n_embd,n_hidden,n_head,n_layer,dropout=0.2):
         super().__init__()
         self.transfo = NoHeadModel(mic_num,tot_timesteps,n_embd,n_hidden,n_head,n_layer,dropout)
@@ -155,6 +188,10 @@ class PredictionModel(nn.Module):
 
 
 class SourcePredictModel(nn.Module):
+    """
+        Predicts the number of sources, from the last embedding of the time-series. Works similarly 
+        to WaveModel, but is designed to be used with a pretrained NoHeadModel.
+    """
     def __init__(self,transf_model,n_sources,n_hidden):
         super().__init__()
         self.transfo=transf_model
@@ -190,7 +227,7 @@ class SourcePredictModel(nn.Module):
         if targets is None:
             loss = None
         else:
-            # predicts = predicts.view(B,2*N,T)
+            # predicts = predicts.reshape(B,2*N,T)
             # B, T, C = logits.shape
 
             targets = targets.reshape(-1,2*self.n_sources)
