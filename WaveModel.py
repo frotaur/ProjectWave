0e088f7bd2daa1e53de841d94f0768ed93e08075
Vassilis <vassilis.papadopoulos@epfl.ch>
2023-06-09 16:34:59 +0200

    A bit of cleaning in the code

--- WaveModel.py
+++ WaveModel.py
@@ -111,6 +111,9 @@ class WaveModel(nn.Module):
         return predicts, loss
     
 
+#============================================================================================
+#==================FOR USE WITH PRETRAINING, DOESNT WORK GREAT IT SEEMS======================
+#============================================================================================
 class NoHeadModel(nn.Module):
     """
     Process the dataseries of mic recordings, but does not process the final output of the transformer
@@ -234,3 +237,89 @@ class SourcePredictModel(nn.Module):
             loss = F.mse_loss(x, targets)+F.mse_loss(predict,x)
         return predict,loss
 
+#============================================================================================
+#=================OTHER EMBEDDINGS OF POSITIONS==============================================
+#============================================================================================
+class WaveModelTesting(nn.Module):
+    """
+        Model that predicts mics location directly from the timeseries data.
+        Not clear yet what the best way to proceed is yet, but for now uses
+        the last 'token' embedding to make the prediciton of the source locations :
+    """
+    def __init__(self,mic_num,source_num,tot_timesteps,n_embd,n_hidden,n_head,n_layer,dropout=0.2):
+        super().__init__()
+        #wavedata:(6,512,3) (3,6,512)(512 3*6) (2,0,1)(3,1,2)
+        eff_n_embd = n_embd
+        self.position_embedding_table = nn.Embedding(tot_timesteps, n_embd)
+
+        self.val_embed = nn.Linear(mic_num,eff_n_embd)
+        self.pos_embed = nn.Linear(mic_num*2,n_embd)
+
+        self.blocks = nn.Sequential(*[Block(n_embd,n_head,n_hidden,dropout) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd) # final layer norm（B,T,n_embd)
+
+        self.lm_head = nn.Sequential(nn.Linear(n_embd*tot_timesteps,n_embd),nn.ReLU(),nn.Linear(n_embd, 2*source_num))
+
+        self.tan=nn.Tanh()
+
+        self.source_num=source_num
+        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+    def forward(self,idx,targets):
+        ## It should take as input the values (B,M,T), the 
+        ## mic positions (B,M,2) and the targets (N,2) (optionally)
+        ## OR, if you do it in the dataset, it will take
+        ## (B,M,T,3) and (N,2) directly.
+
+        #### CHANGES HERE TO BE DONE
+        B,_,M,T = idx.shape #(B,3,M,T)
+        B,N,_= targets.shape
+        #tok_emb = self.token_embedding_table(idx) # (B,T,C)
+        
+        # x = None # (B,T,C) apply a linear layer to get from (B,M,T,3) To（B,T,C)
+        #### HERE x should have shape (B,T,n_embed)
+        idx=torch.permute(idx,(0,3,2,1)) # (B,T,M,3)
+        vals = idx[...,0]
+        pos = idx[:,0,:,1:].reshape(B,M*2) #(B,M*2)
+        idx=idx.reshape(B,T,M*3)
+        # idx=torch.reshape(idx,(B,T*M*3))
+        pos_emb = self.position_embedding_table(torch.arange(T, device= idx.device)[None].expand(B,-1))# (B,T,C)
+
+        idx=self.val_embed(vals)+self.pos_embed(pos)+pos_emb # (B,T,n_embed)
+        
+        idx = self.blocks(idx) # (B,T,C)
+        idx = self.ln_f(idx) # (B,T,C) 
+
+        B,T,C=idx.shape
+        #idx=idx.reshape(B,T*C)
+        
+        idx = self.lm_head(idx).reshape(B,self.source_num,2)#(B,n,2)
+        predicts = torch.clamp(idx,min=0.,max=1.)
+        
+        #predicts = (self.tan(predicts)+1)/2
+        # predicts=torch.permute(predicts,(0,2,1))
+        ### CHANGES HERE AGAIN :
+        # logits = x.reshape(n,2) # (B,T,vocab_size)
+        ### We want to get an 'output' variable, which
+        ### has size (B,N,2)
+        # predicts = torch.permute(predicts,(0,2,1))
+        ### Change this part to compute the mean squared error
+        ### Using the targets
+        if targets is None:
+            loss = None
+        else:
+            # loss = F.mse_loss(predicts,targets)+F.mse_loss(predicts,idx)
+            loss = chamfer_dist(predicts,targets)+F.mse_loss(predicts,idx)
+
+        
+        return predicts, loss
+    
